434bd16912e089e1c3df702ac2c87b6e9a54c6f5
Tara Campbell <tara0campbell@gmail.com>
2014-06-25 14:16:18 -0400

    Updated script to import mri information into CouchDB.

--- tools/CouchDB_Import_MRI.php
+++ tools/CouchDB_Import_MRI.php
@@ -11,26 +11,39 @@ class CouchDBMRIImporter {
     // the code a little more readable.
     var $Dictionary = array(
         'QCComment' => array(
-            'Type' => 'varchar(255)',
-            'Description' => 'QC Comment for Session'
+            'Description' => 'QC Comment for Session',
+            'Type' => 'varchar(255)'
+        ),
+        'QCStatus' => array(
+            'Description' => 'QC Status for Session',
+            'Type' => "enum('','Pass','Fail')"
+        ),
+        'QCPending' => array(
+            'Description' => 'QC Pending for Session',
+            'Type' => "enum('Y','N')"
         )
     );
+
     function __construct() {
         $this->SQLDB = Database::singleton();
         $this->CouchDB = CouchDB::singleton();
     }
 
     function UpdateDataDict($types) {
-        
         foreach($types as $type) {
             $ScanType = $type['ScanType'];
             $this->Dictionary["Selected_$ScanType"] = array(
+                'Description' => "Selected $ScanType file for session",
                 'Type' => 'varchar(255)',
-                'Description' => "Selected $ScanType file for session"
+                'IsFile' => true
             );
             $this->Dictionary[$ScanType . "_QCStatus"] = array(
-                'Type' => "enum('Pass', 'Fail')",
-                'Description' => "QC Status for $ScanType file"
+                'Description' => "QC Status for $ScanType file",
+                'Type' => "enum('Pass', 'Fail')"
+            );
+            $this->Dictionary[$ScanType . "_CaveatEmptor"] = array(
+                'Description' => "Caveat emptor for $ScanType file",
+                'Type' => "enum('Pass', 'Fail')"
             );
         }
         $this->CouchDB->replaceDoc("DataDictionary:mri_data",
@@ -43,15 +56,16 @@ class CouchDBMRIImporter {
     }
 
     function _generateCandidatesQuery($ScanTypes) {
-        $Query = "SELECT c.PSCID, s.Visit_label, fmric.Comment as QCComment";
+        $Query = "SELECT c.PSCID, s.Visit_label, fmric.Comment as QCComment, s.MRIQCStatus as QCStatus, 
+            s.MRIQCPending as QCPending";
         foreach($ScanTypes as $Scan) {
-            $Query .= ", (SELECT f.File FROM files f LEFT JOIN files_qcstatus fqc USING(FileID) LEFT JOIN parameter_file p ON (p.FileID=f.FileID AND p.ParameterTypeID=$Scan[ParameterTypeID]) WHERE f.SessionID=s.ID AND fqc.QCStatus='Pass' AND p.Value='$Scan[ScanType]' LIMIT 1) as `Selected_$Scan[ScanType]`, (SELECT fqc.QCStatus FROM files f LEFT JOIN files_qcstatus fqc USING(FileID) LEFT JOIN parameter_file p ON (p.FileID=f.FileID AND p.ParameterTypeID=$Scan[ParameterTypeID]) WHERE f.SessionID=s.ID AND fqc.QCStatus='Pass' AND p.Value='$Scan[ScanType]' LIMIT 1) as `$Scan[ScanType]_QCStatus`";
+            $Query .= ", (SELECT f.File FROM files f LEFT JOIN files_qcstatus fqc USING(FileID) LEFT JOIN parameter_file p ON (p.FileID=f.FileID AND p.ParameterTypeID=$Scan[ParameterTypeID]) WHERE f.SessionID=s.ID AND fqc.QCStatus='Pass' AND p.Value='$Scan[ScanType]' LIMIT 1) as `Selected_$Scan[ScanType]`, (SELECT fqc.QCStatus FROM files f LEFT JOIN files_qcstatus fqc USING(FileID) LEFT JOIN parameter_file p ON (p.FileID=f.FileID AND p.ParameterTypeID=$Scan[ParameterTypeID]) WHERE f.SessionID=s.ID AND fqc.QCStatus='Pass' AND p.Value='$Scan[ScanType]' LIMIT 1) as `$Scan[ScanType]_QCStatus`, (SELECT CASE WHEN f.Caveat=0 THEN 'False' WHEN f.Caveat=1 THEN 'True' END FROM files f LEFT JOIN files_qcstatus fqc USING(FileID) LEFT JOIN parameter_file p ON (p.FileID=f.FileID AND p.ParameterTypeID=$Scan[ParameterTypeID]) WHERE f.SessionID=s.ID AND fqc.QCStatus='Pass' AND p.Value='$Scan[ScanType]' LIMIT 1) as `$Scan[ScanType]_CaveatEmptor`";
         }
         $Query .= " FROM session s JOIN candidate c USING (CandID) LEFT JOIN feedback_mri_comments fmric ON (fmric.CommentTypeID=7 AND fmric.SessionID=s.ID) WHERE c.PSCID <> 'scanner' AND c.PSCID NOT LIKE '%9999' AND c.Active='Y' AND s.Active='Y' AND c.CenterID <> 1";
         return $Query;
     }
 
-    function UpdateCandidateDocs($data) {
+    function UpdateCandidateDocs($data, $ScanTypes) {
         foreach($data as $row) {
             $doc = $row;
             $identifier = array($row['PSCID'], $row['Visit_label']);
@@ -66,18 +80,69 @@ class CouchDBMRIImporter {
                 'data' => $doc
             ));
             print $docid . ": " . $success . "\n";
+
+            $config = NDB_Config::singleton();
+            $paths = $config->getSetting('paths');
+
+            foreach($ScanTypes as $Scan) {
+                // This isn't very efficient to get the document a second time, but we need
+                // the rev for adding the attachments. This whole section should be
+                // optimized/cleaned up. For now it's just a hack to get the data into
+                // CouchDB, it isn't very clean.
+                // This should all be done using a single multipart request eventually.
+                $latestDoc = $this->CouchDB->getDoc($docid);
+
+                $fileName = $doc['Selected_' . $Scan['ScanType']];
+                $fullPath = $paths['mincPath'] . $fileName;
+                if(file_exists($fullPath)) {
+                    if(!empty($fileName)) {
+                        $toUpload = null;
+                        if(!empty($latestDoc['_attachments'])) {
+                            
+                            if(isset($latestDoc['_attachments'][$fileName])) {
+                                $size = $latestDoc['_attachments'][$fileName]['length'];
+                                if($size != filesize($fullPath)) {
+                                    // File has been modified, upload it.
+                                    $toUpload = $fileName;
+                                }
+                            } 
+                            else {
+                                // This attachment not been uploaded - ever
+                                $toUpload = $fileName;
+                            }
+                        } 
+                        else {
+                            // No current attachments, so this file has not
+                            // been uploaded
+                            $toUpload = $fileName;
+                        }
+                        if(!empty($toUpload)) {
+                            $data = file_get_contents($fullPath);
+                            print "Adding $fileName to $docid\n";
+                            $output = $this->CouchDB->_postRelativeURL($docid . '/' . $fileName . '?rev=' . $latestDoc['_rev'], $data, 'PUT', 'application/x-minc');
+                        }
+                    }
+                } 
+                else {
+                        print "****COULD NOT FIND $fullPath TO ADD TO $docid***\n";
+                }
+            }
         }
         return;
     }
+
     function run() {
-        $ScanTypes = $this->SQLDB->pselect("SELECT DISTINCT pf.ParameterTypeID, pf.Value as ScanType from parameter_type pt JOIN parameter_file pf USING (ParameterTypeID) WHERE pt.Name='selected' AND COALESCE(pf.Value, '') <> ''", array());
+        $ScanTypes = $this->SQLDB->pselect("SELECT DISTINCT pf.ParameterTypeID, pf.Value AS ScanType 
+            FROM parameter_type pt JOIN parameter_file pf USING (ParameterTypeID) 
+            WHERE pt.Name='selected' AND COALESCE(pf.Value, '') <> ''", array());
         $this->UpdateDataDict($ScanTypes);
         $query = $this->_generateCandidatesQuery($ScanTypes);
         $CandidateData = $this->SQLDB->pselect($query, array());
-        $this->UpdateCandidateDocs($CandidateData);
+        $this->UpdateCandidateDocs($CandidateData, $ScanTypes);
     }
 }
-// Don't run if we're doing the unit tests, the unit test will call run..
+
+// Don't run if we're doing the unit tests; the unit test will call run.
 if(!class_exists('UnitTestCase')) {
     $Runner = new CouchDBMRIImporter();
     $Runner->run();
